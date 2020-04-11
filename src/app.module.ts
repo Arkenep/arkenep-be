b23dfea915c43d0bceaf08309c57a423a29b690a
Daumantas Girininkas <ssliteris@deloitte.com>
2020-04-12 00:53:49 +0300

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
+import { config } from 'dotenv';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { TheatersModule } from './theaters/theaters.module';
+config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://arkenep:retCxOAg2HwVTL1l@arkenepdb-dgarn.mongodb.net/sample_mflix?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.MONGO_HOST),
     CatsModule,
     TheatersModule,
   ],
